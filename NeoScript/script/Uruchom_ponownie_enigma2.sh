1be45b786b0b916b76d2c34dbb6ee95cb31fbbcc
gutosie <krzysztofgutosie@gmail.com>
2022-12-13 16:14:57 +0200

    Add files via upload

--- NeoScript/script/Uruchom_ponownie_enigma2.sh
+++ NeoScript/script/Uruchom_ponownie_enigma2.sh
@@ -7,12 +7,12 @@ fi
 
 [ $PL ] && echo "Restart listy kanałów..." || echo "Activating a new list...";    
 sleep 2
-wget -qO - http://127.0.0.1/web/servicelistreload?mode=02 >/dev/null
+wget -qO - http://127.0.0.1/web/servicelistreload?mode=02 >/dev/null >/dev/null
 
 sleep 2    
 [ $PL ] && echo "Lista kanałów została zaktualizowana" || echo "List updated successfully.";
 
-wget -q -O /dev/null http://127.0.0.1/web/servicelistreload?mode=0 2>/dev/null
+wget -q -O /dev/null http://127.0.0.1/web/servicelistreload?mode=0 2 >/dev/null
 
 [ $PL ] && echo "Restart systemu E2 za 5 sekund..." || echo "Restart E2...";
 
