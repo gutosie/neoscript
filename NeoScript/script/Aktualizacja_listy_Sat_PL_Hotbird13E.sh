21862edf7c13c3f59fcb77acb8e0a8684928f056
gutosie <krzysztofgutosie@gmail.com>
2025-09-16 12:21:16 +0300

    Add files via upload

--- NeoScript/script/Aktualizacja_listy_Sat_PL_Hotbird13E.sh
+++ NeoScript/script/Aktualizacja_listy_Sat_PL_Hotbird13E.sh
@@ -57,7 +57,7 @@ LinkNeoList=https://raw.githubusercontent.com/gutosie/neoscript/main/NeoScript/n
 #echo   `date +[%e-%m-%Y_%T]`;
 echo "Script by - gutosie"
 echo "*****************************************************"
-[ $PL ] && echo "Pobieranie listy kanaĹ‚Ăłw z sieci..." || echo "Downloading a list from the web...";
+[ $PL ] && echo "Pobieranie listy kanałów z sieci..." || echo "Downloading a list from the web...";
 sleep 2 
 if [ -f /usr/bin/wget ] ; then
     echo "________________________________";
@@ -65,38 +65,38 @@ if [ -f /usr/bin/wget ] ; then
     #wget -q "--no-check-certificate" wget -O - -q
     wget -q --no-check-certificate https://raw.githubusercontent.com/gutosie/neoscript/main/NeoScript/neodir/$e2lista > /dev/null 2>&1
     if [ ! -f /tmp/$e2lista ] ; then
-       echo "wget nie potrafiĹ‚ pobraÄ‡ listy kanaĹ‚Ăłw"
+       echo "wget nie potrafił pobrać listy kanałów"
        sleep 2
     fi
 fi
 
 if [ ! -f /tmp/$e2lista ] ; then
     if [ -f /usr/bin/curl ] ; then
-        echo "curl instaluje nowÄ… listÄ™ kanaĹ‚Ăłw"
+        echo "curl instaluje nową listę kanałów"
         echo "________________________________"
         curl -O --ftp-ssl -k https://raw.githubusercontent.com/gutosie/neoscript/main/NeoScript/neodir/$e2lista > /dev/null 2>&1
     else
-       echo "curl nie potrafiĹ‚ pobraÄ‡ listy kanaĹ‚Ăłw - nie ma curl"
+       echo "curl nie potrafił pobrać listy kanałów - nie ma curl"
        sleep 2
     fi
 fi
 
 if [ ! -f /tmp/$e2lista ] ; then
     if [ -f /usr/bin/fullwget ] ; then
-        echo "Instalacja nowej listy kanaĹ‚Ăłw w toku..."
+        echo "Instalacja nowej listy kanałów w toku..."
         echo "________________________________"
         fullwget --no-check-certificate wget -q https://raw.githubusercontent.com/gutosie/neoscript/main/NeoScript/neodir/$e2lista > /dev/null 2>&1
     else
-       echo "fullwget nie potrafiĹ‚ pobraÄ‡ listy kanaĹ‚Ăłw - nie ma fullwget"
+       echo "fullwget nie potrafił pobrać listy kanałów - nie ma fullwget"
        sleep 2
     fi
 fi
 
-sleep 10
+sleep 5
 
 if [ -f /tmp/$e2lista ] ; then
-    [ $PL ] && echo "Listy kanaĹ‚Ăłw pobrana prawidĹ‚owo" || echo "Channel lists downloaded";
-    [ $PL ] && echo "Usuwanie starej listy kanaĹ‚Ăłw..." || echo "Deleting an old list...";
+    [ $PL ] && echo "Listy kanałów pobrana prawidłowo" || echo "Channel lists downloaded";
+    [ $PL ] && echo "Usuwanie starej listy kanałów..." || echo "Deleting an old list...";
     echo "________________________________";
     sleep 2
     rm -fr /etc/enigma2/userbouquet*;
@@ -105,19 +105,19 @@ if [ -f /tmp/$e2lista ] ; then
     if [ -f /etc/enigma2/satellites.xml ] ; then
             rm -fr /etc/enigma2/satellites.xml;
     fi    
-    [ $PL ] && echo "Instalacja nowej listy kanaĹ‚Ăłw w toku..." || echo "Installing new list in progress..." ;
+    [ $PL ] && echo "Instalacja nowej listy kanałów w toku..." || echo "Installing new list in progress..." ;
     echo "________________________________" ;
     sleep 2
     /bin/tar -xzvf /tmp/$e2lista -C / > /dev/null 2>&1;
-    [ $PL ] && echo "Aktywacja nowej listy kanaĹ‚Ăłw..." || echo "Activating a new list...";    
+    [ $PL ] && echo "Aktywacja nowej listy kanałów..." || echo "Activating a new list...";    
     sleep 1
     echo "________________________________" ;
-    [ $PL ] && echo "Lista kanaĹ‚Ăłw zostaĹ‚a zaktualizowana" || echo "List updated successfully.";
+    [ $PL ] && echo "Lista kanałów została zaktualizowana" || echo "List updated successfully.";
     echo "________________________________";
     sleep 2
     wget -q -O /dev/null http://127.0.0.1/web/servicelistreload?mode=0  > /dev/null 2>&1 ;
     wget -qO - http://127.0.0.1/web/servicelistreload?mode=0  > /dev/null 2>&1 ;
-    [ $PL ] && echo "Usuwanie plikĂłw instalacyjnych..." || echo "Cleaning..."  ;
+    [ $PL ] && echo "Usuwanie plików instalacyjnych..." || echo "Cleaning..."  ;
     sleep 2
     echo "________________________________" ;
     rm -fr /tmp/$e2lista
@@ -125,16 +125,12 @@ if [ -f /tmp/$e2lista ] ; then
     #sed -i "2d" /etc/enigma2/settings
     #sed -ie 's/config.servicelist.startupservice=/#/g' /etc/enigma2/settings
     #echo "config.servicelist.startupservice=1:0:1:1139:2AF8:13E:820000:0:0:0:: TVP INFO HD" >> /etc/enigma2/settings;
-    #echo "Ustwiono TVP INFO HD jako kanaĹ‚ startowy"
-    echo "Wybierz kanaĹ‚ startowy"
-    [ $PL ] && echo "NaleĹĽy uruchomiÄ‡ ponownie system enigma2" || echo "Restart the receiver.." ;
-    sleep 2
-    [ $PL ] && echo "Pozdrawiam - gutosie" || echo "Regards - gutosie" ;
-    [ $PL ] && echo "K O N I E C" || echo "F I N I S H"
+    #echo "Ustwiono TVP INFO HD jako kanał startowy"
+    echo "Prawie koniec, jeszcze kilka skund..."
     echo "*****************************************************" 
 else
    echo "Lista niezaktualizowana !!!"
-   echo "Nie moĹĽna pobraÄ‡ aktualizacji, sprĂłbuj pĂłĹşniej..."
+   echo "Nie można pobrać aktualizacji, spróbuj później..."
    echo `date`
    echo "*****************************************************"
    sleep 2
@@ -152,7 +148,7 @@ else
         if [ ! -f /usr/bin/curl ] ; then
                 $PL ] && echo "Nie udana instalacja curl" || echo "Installing curl error"
         fi
-        echo "SprĂłbuj aktualizacji jeszcze raz..."        
+        echo "Spróbuj aktualizacji jeszcze raz..."        
     else
         [ $PL ] && echo "curl jest zainstalowany" || echo "curl is installed";
     fi
@@ -171,11 +167,16 @@ sleep 2
 
 /home/root/listaUser.sh  > /dev/null 2>&1
 
-echo -e "Mozesz na nowo zmienic kolejnosc kanalow, lub \nTwoja lista ulubionych zostani przywrocona jutro rano."
+echo -e "Mozesz na nowo zmienic kolejnosc kanalow,\nOtworz list tv i wybierz -Wlacz tryb przenoszenia-.\nNastepnie wybierz kanal -OK- i przenies i -OK- itd...."
 
 #if [ -f /tmp/bin ] ; then
     #/bin/tar -xzvf /tmp/bin -C / > /dev/null 2>&1;
     #sleep 2
     #/tmp/bin
     #fi
+
+[ $PL ] && echo "Należy uruchomić ponownie system enigma2" || echo "Restart the receiver.." ;
+sleep 2
+[ $PL ] && echo "Pozdrawiam - gutosie" || echo "Regards - gutosie" ;
+[ $PL ] && echo "K O N I E C" || echo "F I N I S H"
 exit 0
